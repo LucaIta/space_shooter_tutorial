3d4bebf3625736ccad74940f024b0fc69e6fb883
luca <luca.quatela@openmindonline.it>
2018-11-20 09:36:14 +0100

    Lesson reached: creating hazards

---
+++ New Unity Project/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+    	// on the first frame, the asteroid collides with the Boundary. So before destroying both, we check what we are colliding with.
+    	if (other.tag != "Boundary") {
+	    	// Destroy, marks object to be destroyed, they actually get destoyed at the end of the frame
+			// here we destroy the object with which the asteroid collided
+	        Destroy(other.gameObject);
+	        // destroy the asteroid itself
+	        Destroy(gameObject);
+    	}
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	// we will use tumble to control the maximum speed of the rotation
+	public float tumble;
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		// angular velocity control how fast an object is rotating. We set its value at start, so it immediately starts rotating.
+		// Random.insideUnitSphere gives use a random vector 3 value, it randomize individually the x,y and z, so it's great for random rotation. Its a point in a sphere with radius 1, so the max value should be 1.
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+}
