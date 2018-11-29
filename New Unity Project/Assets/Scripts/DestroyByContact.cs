6013f6197b84d3e0b4f1ef4d7b76a031065ce267
luca <luca.quatela@openmindonline.it>
2018-11-29 09:04:34 +0100

    add files as for lesson 5.3 - Explosions

--- New Unity Project/Assets/Scripts/DestroyByContact.cs
+++ New Unity Project/Assets/Scripts/DestroyByContact.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     // Destroy everything that enters the trigger
     void OnTriggerEnter(Collider other)
     {
     	// on the first frame, the asteroid collides with the Boundary. So before destroying both, we check what we are colliding with.
     	if (other.tag != "Boundary") {
-	    	// Destroy, marks object to be destroyed, they actually get destoyed at the end of the frame
-			// here we destroy the object with which the asteroid collided
-	        Destroy(other.gameObject);
+            // Destroy, marks object to be destroyed, they actually get destoyed at the end of the frame
+            // here we destroy the object with which the asteroid collided
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(other.gameObject);
 	        // destroy the asteroid itself
 	        Destroy(gameObject);
+            if (other.tag == "Player") {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            }
     	}
     }
 }
--- New Unity Project/Assets/Scripts/mover.cs
+++ New Unity Project/Assets/Scripts/mover.cs
@@ -8,11 +8,9 @@ public class mover : MonoBehaviour {
 
     private Rigidbody rb;
 
-
 	private void Start() 
 	{
 		rb = GetComponent<Rigidbody>();
 		rb.velocity = transform.forward * speed;
-		// Debug.Log("transform.forward * speed = " + transform.forward * speed);
 	}
 }
