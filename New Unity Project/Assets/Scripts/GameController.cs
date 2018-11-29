9ede9fc35384ce5ec1befca5f0220fbd3226b921
luca <luca.quatela@openmindonline.it>
2018-11-29 14:07:18 +0100

    add files as for lesson 5.4 - Game Controller

---
+++ New Unity Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	void Start() {
+		SpawnWaves();
+	}
+
+	void SpawnWaves() {
+		// X position must be random, while Y and Z can be set from the IDE
+		Vector3 spawnPosition = new Vector3(Random.Range (- spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate(hazard, spawnPosition, spawnRotation);
+	}
+
+}
